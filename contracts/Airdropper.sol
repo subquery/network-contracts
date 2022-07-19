90647567d2b703366e9de69411fcb778bcdc3dc2
Jianzhuo Shen <shenjianzhuo@gmail.com>
2022-07-20 04:04:00 +0800

    add events and batchClaimAirdrop

--- contracts/Airdropper.sol
+++ contracts/Airdropper.sol
@@ -4,8 +4,8 @@
 pragma solidity ^0.8.10;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 
 contract Airdropper is Ownable {
     using SafeERC20 for IERC20;
@@ -14,7 +14,7 @@ contract Airdropper is Ownable {
     struct Round {
         address tokenAddress; //airdrop token address
         uint256 roundStartTime; //strat time for this round
-        uint256 roundDeadline; //deadline for this round 
+        uint256 roundDeadline; //deadline for this round
         uint256 unclaimedAmount;
     }
 
@@ -23,51 +23,70 @@ contract Airdropper is Ownable {
     uint256 public nextRoundId;
     address public settleDestination;
 
-    event RoundCreated(uint256 indexed roundId);
+    event RoundCreated(uint256 indexed roundId, address tokenAddress, uint256 roundStartTime, uint256 roundDeadline);
 
     event AddAirdrop(address indexed addr, uint256 roundId, uint256 amount);
 
     event AirdropClaimed(address indexed addr, uint256 roundId, uint256 amount);
 
-    event RoundSettled(uint256 indexed roundId, address settleDestination);
+    event RoundSettled(uint256 indexed roundId, address settleDestination, uint256 unclaimAmount);
 
     function setSettleDestination(address _settleDestination) external onlyOwner {
         settleDestination = _settleDestination;
     }
-    
-    function createRound(address _tokenAddr, uint256 _roundStratTime, uint256 _roundDeadline) external onlyOwner returns (uint256) {
+
+    function createRound(
+        address _tokenAddr,
+        uint256 _roundStratTime,
+        uint256 _roundDeadline
+    ) external onlyOwner returns (uint256) {
         require(_roundStratTime > block.timestamp && _roundDeadline > _roundStratTime, 'invaild round time set');
         require(_tokenAddr != address(0), 'invaild token address');
         roundRecord[nextRoundId] = Round(_tokenAddr, _roundStratTime, _roundDeadline, 0);
         nextRoundId += 1;
-        emit RoundCreated(nextRoundId - 1);
+        emit RoundCreated(nextRoundId - 1, _tokenAddr, _roundStratTime, _roundDeadline);
         return nextRoundId - 1;
     }
 
-    function _airdrop(address _addr, uint256 _roundId, uint256 _amount) private {
+    function _airdrop(
+        address _addr,
+        uint256 _roundId,
+        uint256 _amount
+    ) private {
         require(roundRecord[_roundId].roundStartTime > block.timestamp, 'invaild round to airdrop');
         require(airdropRecord[_addr][_roundId] == 0, 'duplicate airdrop');
         require(_amount > 0, 'invaild airdrop amount');
-        
+
         IERC20(roundRecord[_roundId].tokenAddress).safeTransferFrom(msg.sender, address(this), _amount);
         airdropRecord[_addr][_roundId] = _amount;
         roundRecord[_roundId].unclaimedAmount += _amount;
         emit AddAirdrop(_addr, _roundId, _amount);
     }
 
-    function batchAirdrop(address[] calldata _addr, uint256[] calldata _roundId, uint256[] calldata _amount) external onlyOwner {
+    function batchAirdrop(
+        address[] calldata _addr,
+        uint256[] calldata _roundId,
+        uint256[] calldata _amount
+    ) external onlyOwner {
         require(_addr.length == _roundId.length && _addr.length == _amount.length, 'invaild parameters');
         for (uint256 i = 0; i < _addr.length; i++) {
             _airdrop(_addr[i], _roundId[i], _amount[i]);
-        } 
+        }
     }
 
     function claimAirdrop(uint256 _roundId) public {
-        require(roundRecord[_roundId].roundDeadline > block.timestamp && roundRecord[_roundId].roundStartTime < block.timestamp, 'invaild round to claim');
+        require(
+            roundRecord[_roundId].roundDeadline > block.timestamp &&
+                roundRecord[_roundId].roundStartTime < block.timestamp,
+            'invaild round to claim'
+        );
         uint256 amount = airdropRecord[msg.sender][_roundId];
         require(amount != 0, 'nothing claim');
 
-        require(IERC20(roundRecord[_roundId].tokenAddress).balanceOf(address(this)) >= amount, 'Airdropper: insufficient assets');
+        require(
+            IERC20(roundRecord[_roundId].tokenAddress).balanceOf(address(this)) >= amount,
+            'Airdropper: insufficient assets'
+        );
         IERC20(roundRecord[_roundId].tokenAddress).safeTransfer(msg.sender, amount);
         airdropRecord[msg.sender][_roundId] = 0;
 
@@ -75,13 +94,26 @@ contract Airdropper is Ownable {
         emit AirdropClaimed(msg.sender, _roundId, amount);
     }
 
-    function settleEndedRound(uint256 _roundId) public {
-        require(roundRecord[_roundId].roundDeadline < block.timestamp && roundRecord[_roundId].roundDeadline != 0, 'invaild round to settle');
-        require(roundRecord[_roundId].unclaimedAmount != 0, 'none token left');
-        require(IERC20(roundRecord[_roundId].tokenAddress).balanceOf(address(this)) >= roundRecord[_roundId].unclaimedAmount, 'Airdropper: insufficient assets');
-        IERC20(roundRecord[_roundId].tokenAddress).safeTransfer(settleDestination, roundRecord[_roundId].unclaimedAmount);
-        roundRecord[_roundId].unclaimedAmount = 0;
-        emit RoundSettled(_roundId, settleDestination);
+    function batchClaimAirdrop(uint256[] calldata _roundIds) public {
+        for (uint256 i = 0; i < _roundIds.length; i++) {
+            claimAirdrop(_roundIds[i]);
+        }
     }
 
+    function settleEndedRound(uint256 _roundId) public {
+        require(roundRecord[_roundId].roundDeadline < block.timestamp, 'invaild round to settle');
+        uint256 unclaimAmount = roundRecord[_roundId].unclaimedAmount;
+        require(unclaimAmount != 0, 'none token left');
+        require(
+            IERC20(roundRecord[_roundId].tokenAddress).balanceOf(address(this)) >=
+                roundRecord[_roundId].unclaimedAmount,
+            'Airdropper: insufficient assets'
+        );
+        IERC20(roundRecord[_roundId].tokenAddress).safeTransfer(
+            settleDestination,
+            roundRecord[_roundId].unclaimedAmount
+        );
+        roundRecord[_roundId].unclaimedAmount = 0;
+        emit RoundSettled(_roundId, settleDestination, unclaimAmount);
+    }
 }
--- test/Airdropper.test.ts
+++ test/Airdropper.test.ts
@@ -5,7 +5,7 @@ import {expect} from 'chai';
 import {ethers, waffle} from 'hardhat';
 import {deployContracts} from './setup';
 import {Airdropper, Settings, SQToken} from '../src';
-import { ZERO_ADDRESS } from './constants';
+import {ZERO_ADDRESS} from './constants';
 import {etherParse, futureTimestamp, timeTravel, lastestTime} from './helper';
 
 describe('Airdropper Contract', () => {
@@ -26,101 +26,151 @@ describe('Airdropper Contract', () => {
     });
 
     describe('init states check', () => {
-        it('init nextRoundId should be 0',async () => {
+        it('init nextRoundId should be 0', async () => {
             expect(await airdropper.nextRoundId()).to.equal(0);
         });
-        it('init settleDestination should be 0x00',async () => {
+        it('init settleDestination should be 0x00', async () => {
             expect(await airdropper.settleDestination()).to.equal(ZERO_ADDRESS);
         });
     });
 
     describe('config contract', () => {
-        it('set settleDestination should work',async () => {
+        it('set settleDestination should work', async () => {
             await airdropper.setSettleDestination(wallet_3.address);
             expect(await airdropper.settleDestination()).to.equal(wallet_3.address);
         });
     });
 
     describe('round test', () => {
-        it('create round should work',async () => {
-            await airdropper.createRound(sqtAddress, await lastestTime(mockProvider)+60*60, await futureTimestamp(mockProvider, 60*60*24*3));
+        it('create round should work', async () => {
+            await airdropper.createRound(
+                sqtAddress,
+                (await lastestTime(mockProvider)) + 60 * 60,
+                await futureTimestamp(mockProvider, 60 * 60 * 24 * 3)
+            );
             expect((await airdropper.roundRecord(0)).tokenAddress).to.equal(sqtAddress);
             expect(await airdropper.nextRoundId()).to.equal(1);
         });
-        it('create round with invaild parameter should fail',async () => {
-            await expect(airdropper.createRound(ZERO_ADDRESS, await lastestTime(mockProvider)+60*60, await futureTimestamp(mockProvider, 60*60*24*3))).to.be.revertedWith('invaild token address');
-            await expect(airdropper.createRound(sqtAddress, 100, await futureTimestamp(mockProvider, 60*60*24*3))).to.be.revertedWith('invaild round time set');
-            await expect(airdropper.createRound(sqtAddress, await lastestTime(mockProvider)+60*60*2, await lastestTime(mockProvider)+60*60)).to.be.revertedWith('invaild round time set');
+        it('create round with invaild parameter should fail', async () => {
+            await expect(
+                airdropper.createRound(
+                    ZERO_ADDRESS,
+                    (await lastestTime(mockProvider)) + 60 * 60,
+                    await futureTimestamp(mockProvider, 60 * 60 * 24 * 3)
+                )
+            ).to.be.revertedWith('invaild token address');
+            await expect(
+                airdropper.createRound(sqtAddress, 100, await futureTimestamp(mockProvider, 60 * 60 * 24 * 3))
+            ).to.be.revertedWith('invaild round time set');
+            await expect(
+                airdropper.createRound(
+                    sqtAddress,
+                    (await lastestTime(mockProvider)) + 60 * 60 * 2,
+                    (await lastestTime(mockProvider)) + 60 * 60
+                )
+            ).to.be.revertedWith('invaild round time set');
         });
     });
 
     describe('airdrop test', () => {
         beforeEach(async () => {
-            //roundId 0: expire 
-            await airdropper.createRound(sqtAddress, await futureTimestamp(mockProvider, 60*60*2), await futureTimestamp(mockProvider, 60*60*24*2));
-            //roundId 1: ongoing 
-            await airdropper.createRound(sqtAddress, await futureTimestamp(mockProvider, 60*60*3), await futureTimestamp(mockProvider, 60*60*24*5));
+            //roundId 0: expire
+            await airdropper.createRound(
+                sqtAddress,
+                await futureTimestamp(mockProvider, 60 * 60 * 2),
+                await futureTimestamp(mockProvider, 60 * 60 * 24 * 2)
+            );
+            //roundId 1: ongoing
+            await airdropper.createRound(
+                sqtAddress,
+                await futureTimestamp(mockProvider, 60 * 60 * 3),
+                await futureTimestamp(mockProvider, 60 * 60 * 24 * 5)
+            );
 
-            await token.increaseAllowance(airdropper.address, etherParse("100"));
+            await token.increaseAllowance(airdropper.address, etherParse('100'));
         });
-        it('batch airdrop should work',async () => {
-            await airdropper.connect(wallet_0).batchAirdrop([wallet_1.address, wallet_2.address], [1, 1], [etherParse("10"), etherParse("20")]);
-            expect(await airdropper.airdropRecord(wallet_1.address, 1)).to.be.eq(etherParse("10"));
-            expect(await airdropper.airdropRecord(wallet_2.address, 1)).to.be.eq(etherParse("20"));
-            expect(await (await airdropper.roundRecord(1)).unclaimedAmount).to.be.eq(etherParse("30"));
-            expect(await token.balanceOf(airdropper.address)).to.be.eq(etherParse("30"));
+        it('batch airdrop should work', async () => {
+            await airdropper
+                .connect(wallet_0)
+                .batchAirdrop([wallet_1.address, wallet_2.address], [1, 1], [etherParse('10'), etherParse('20')]);
+            expect(await airdropper.airdropRecord(wallet_1.address, 1)).to.be.eq(etherParse('10'));
+            expect(await airdropper.airdropRecord(wallet_2.address, 1)).to.be.eq(etherParse('20'));
+            expect(await (await airdropper.roundRecord(1)).unclaimedAmount).to.be.eq(etherParse('30'));
+            expect(await token.balanceOf(airdropper.address)).to.be.eq(etherParse('30'));
         });
-        it('duplicate airdrop should fail',async () => {
-            await airdropper.batchAirdrop([wallet_1.address], [1], [etherParse("10")]);
-            await expect(airdropper.batchAirdrop([wallet_1.address], [1], [etherParse("20")])).to.be.revertedWith('duplicate airdrop');
+        it('duplicate airdrop should fail', async () => {
+            await airdropper.batchAirdrop([wallet_1.address], [1], [etherParse('10')]);
+            await expect(airdropper.batchAirdrop([wallet_1.address], [1], [etherParse('20')])).to.be.revertedWith(
+                'duplicate airdrop'
+            );
         });
-        it('airdrop invaild round should fail',async () => {
-            await timeTravel(mockProvider, 60*60*2);
-            await expect(airdropper.batchAirdrop([wallet_1.address], [0], [etherParse("10")])).to.be.revertedWith('invaild round to airdrop');
+        it('airdrop invaild round should fail', async () => {
+            await timeTravel(mockProvider, 60 * 60 * 2);
+            await expect(airdropper.batchAirdrop([wallet_1.address], [0], [etherParse('10')])).to.be.revertedWith(
+                'invaild round to airdrop'
+            );
         });
-        it('airdrop 0 amount should fail',async () => {
-            await expect(airdropper.batchAirdrop([wallet_1.address], [1], [etherParse("0")])).to.be.revertedWith('invaild airdrop amount');
+        it('airdrop 0 amount should fail', async () => {
+            await expect(airdropper.batchAirdrop([wallet_1.address], [1], [etherParse('0')])).to.be.revertedWith(
+                'invaild airdrop amount'
+            );
         });
-        it('settle invaild round should work',async () => {
+        it('settle invaild round should work', async () => {
             await airdropper.setSettleDestination(wallet_3.address);
-            await airdropper.batchAirdrop([wallet_1.address], [0], [etherParse("10")]);
+            await airdropper.batchAirdrop([wallet_1.address], [0], [etherParse('10')]);
             await expect(airdropper.settleEndedRound(0)).to.be.revertedWith('invaild round to settle');
-            await timeTravel(mockProvider, 60*60*24*3);
+            await timeTravel(mockProvider, 60 * 60 * 24 * 3);
             await airdropper.settleEndedRound(0);
-            expect(await token.balanceOf(airdropper.address)).to.be.eq(etherParse("0"));
+            expect(await token.balanceOf(airdropper.address)).to.be.eq(etherParse('0'));
             await expect(airdropper.settleEndedRound(0)).to.be.revertedWith('none token left');
-            expect(await token.balanceOf(wallet_3.address)).to.be.eq(etherParse("10"));
+            expect(await token.balanceOf(wallet_3.address)).to.be.eq(etherParse('10'));
         });
     });
 
     describe('claim airdrop test', () => {
         beforeEach(async () => {
-            await token.increaseAllowance(airdropper.address, etherParse("100"));
-            await airdropper.createRound(sqtAddress, await futureTimestamp(mockProvider, 60*60*2), await futureTimestamp(mockProvider, 60*60*24*2));
-            await airdropper.batchAirdrop([wallet_1.address], [0], [etherParse("10")]);
-            await airdropper.batchAirdrop([wallet_2.address], [0], [etherParse("20")]);
+            await token.increaseAllowance(airdropper.address, etherParse('100'));
+            await airdropper.createRound(
+                sqtAddress,
+                await futureTimestamp(mockProvider, 60 * 60 * 2),
+                await futureTimestamp(mockProvider, 60 * 60 * 24 * 2)
+            );
+            await airdropper.createRound(
+                sqtAddress,
+                await futureTimestamp(mockProvider, 60 * 60 * 2),
+                await futureTimestamp(mockProvider, 60 * 60 * 24 * 2)
+            );
+            await airdropper.batchAirdrop(
+                [wallet_1.address, wallet_1.address],
+                [0, 1],
+                [etherParse('10'), etherParse('20')]
+            );
         });
-        it('claim airdrop should work',async () => {
-            await timeTravel(mockProvider, 60*60*3);
+        it('claim airdrop should work', async () => {
+            await timeTravel(mockProvider, 60 * 60 * 3);
             await airdropper.connect(wallet_1).claimAirdrop(0);
-            expect(await airdropper.airdropRecord(wallet_1.address, 0)).to.be.eq(etherParse("0"));
-            expect(await (await airdropper.roundRecord(0)).unclaimedAmount).to.be.eq(etherParse("20"));
-            expect(await token.balanceOf(airdropper.address)).to.be.eq(etherParse("20"));
-            expect(await token.balanceOf(wallet_1.address)).to.be.eq(etherParse("10"));
+            expect(await airdropper.airdropRecord(wallet_1.address, 0)).to.be.eq(etherParse('0'));
+            expect(await (await airdropper.roundRecord(0)).unclaimedAmount).to.be.eq(etherParse('0'));
+            expect(await token.balanceOf(airdropper.address)).to.be.eq(etherParse('20'));
+            expect(await token.balanceOf(wallet_1.address)).to.be.eq(etherParse('10'));
         });
-        it('claim 0 airdrop should fail',async () => {
-            await timeTravel(mockProvider, 60*60*3);
+        it('batch claim airdrop should work', async () => {
+            await timeTravel(mockProvider, 60 * 60 * 3);
+            await airdropper.connect(wallet_1).batchClaimAirdrop([0, 1]);
+            expect(await airdropper.airdropRecord(wallet_1.address, 0)).to.be.eq(etherParse('0'));
+            expect(await (await airdropper.roundRecord(0)).unclaimedAmount).to.be.eq(etherParse('0'));
+            expect(await token.balanceOf(airdropper.address)).to.be.eq(etherParse('0'));
+            expect(await token.balanceOf(wallet_1.address)).to.be.eq(etherParse('30'));
+        });
+        it('claim 0 airdrop should fail', async () => {
+            await timeTravel(mockProvider, 60 * 60 * 3);
             await airdropper.connect(wallet_1).claimAirdrop(0);
             await expect(airdropper.connect(wallet_1).claimAirdrop(0)).to.be.revertedWith('nothing claim');
         });
-        it('claim invaild round should fail',async () => {
+        it('claim invaild round should fail', async () => {
             await expect(airdropper.connect(wallet_1).claimAirdrop(0)).to.be.revertedWith('invaild round to claim');
-            await timeTravel(mockProvider, 60*60*24*3);
+            await timeTravel(mockProvider, 60 * 60 * 24 * 3);
             await expect(airdropper.connect(wallet_1).claimAirdrop(0)).to.be.revertedWith('invaild round to claim');
         });
-
     });
-
-
-
 });
