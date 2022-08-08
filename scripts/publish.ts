9f1cd4366ae8861344ca9b92c9c2ec50d3f60bc5
Jianzhuo Shen <shenjianzhuo@gmail.com>
2022-08-08 17:41:26 +0800

    add github workflow to publish contracts

---
+++ scripts/publish.ts
@@ -0,0 +1,66 @@
+import dotenv from 'dotenv';
+import {ApiPromise, WsProvider} from '@polkadot/api';
+import {keyring} from '@polkadot/ui-keyring';
+import fs, {writeFileSync} from 'fs';
+import {DeploymentConfig} from '../src/types';
+import localConfig from './config/local.config';
+import testnetConfig from './config/testnet.config';
+import mainnetConfig from './config/mainnet.config';
+
+dotenv.config();
+
+const main = async () => {
+    let config: DeploymentConfig;
+
+    switch (process.argv[2]) {
+        case '--mainnet':
+            config = mainnetConfig as DeploymentConfig;
+            break;
+        case '--testnet':
+            config = testnetConfig as DeploymentConfig;
+            break;
+        default:
+            config = localConfig();
+    }
+
+    const seed = process.env.SEED;
+
+    const wsProvider = new WsProvider('wss://acala-mandala.api.onfinality.io/public-ws');
+    const api = await ApiPromise.create({provider: wsProvider});
+
+    keyring.loadAll({ss58Format: 42, type: 'sr25519'});
+    const {pair, json} = keyring.addUri(seed, 'password', {name: 'subquery acc'});
+
+    const filePath = `${__dirname}/../publish/${config.network.name}.json`;
+    let deployment = JSON.parse(fs.readFileSync(filePath, {encoding: 'utf8'}));
+
+    for (var key in deployment) {
+        if (deployment.hasOwnProperty(key)) {
+            const value = deployment[key];
+            const is_published = (await api.query.evm.accounts(value.address)).toJSON().contractInfo.published;
+
+            if (is_published) {
+                console.log(`${key} is published`);
+            } else {
+                console.log(`start publish ${key} : ${value.address}`);
+
+                await new Promise(function (resolve, reject) {
+                    api.tx.evm.publishContract(value.address).signAndSend(pair, ({events = [], status}) => {
+                        if (status.isFinalized) {
+                            events.forEach(({event: {data, method, section}, phase}) => {
+                                if (method == 'ExtrinsicSuccess') {
+                                    console.log(`${key} published success`);
+                                } else if (method == 'ExtrinsicFailed') {
+                                    console.log(`${key} published fail`);
+                                }
+                            });
+                            resolve(true);
+                        }
+                    });
+                });
+            }
+        }
+    }
+};
+
+main();
