6b7b4afcecd867eb881fd52a07dab4366f640925
HZ <zhaihaohk@gmail.com>
2022-08-22 10:36:00 +1200

    Update pipeline to support deploy to moonbase

--- scripts/config/testnet.config.ts
+++ scripts/config/testnet.config.ts
@@ -1,11 +1,11 @@
 export default {
     network: {
         name: 'testnet',
+        platform: 'acala',
         endpoint: {
             eth: 'https://acala-mandala-adapter.api.onfinality.io/public',
             substrate: 'wss://acala-mandala.api.onfinality.io/public-ws',
         },
-        platform: 'acala',
     },
     contracts: {
         InflationController: [1000, '0x4ae8fcdddc859e2984ce0b8f4ef490d61a7a9b7f'], // inflationRate, inflationDestination
