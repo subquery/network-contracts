6a020fec1c0ae70f457aab29d64b9d556858746a
Neo Sun <sun@subquery.network>
2022-08-31 15:20:58 +1200

    fix build script

--- build.rs
+++ build.rs
@@ -1,20 +1,9 @@
 // Build contracts and rust.
-use std::env;
-use std::path::Path;
 use std::process::Command;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    println!("Installing contracts={}", out_dir);
-    Command::new("yarn")
-        .args(&["install"])
-        .current_dir(&Path::new(&out_dir))
-        .status()
-        .unwrap();
-    println!("Building contracts={}", out_dir);
-    Command::new("yarn")
-        .args(&["build"])
-        .current_dir(&Path::new(&out_dir))
-        .status()
-        .unwrap();
+    println!("Installing contracts...");
+    Command::new("yarn").args(&["install"]).status().unwrap();
+    println!("Building contracts...");
+    Command::new("yarn").args(&["build"]).status().unwrap();
 }
