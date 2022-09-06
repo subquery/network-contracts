7a173de3789e323923bf4345ab12a72a8b1b147f
Jianzhuo Shen <shenjianzhuo@gmail.com>
2022-09-06 22:56:18 +0800

    update deploy config

--- scripts/config/mainnet.config.ts
+++ scripts/config/mainnet.config.ts
@@ -1,10 +1,13 @@
 export default {
     network: {
-        name: "mainnet",
-        platform: "acala"
+        name: 'mainnet',
+        platform: 'acala',
     },
     contracts: {
-        InflationController: [], // inflationRateBP, inflationDestination
+        InflationController: [1000, '0x4ae8fcdddc859e2984ce0b8f4ef490d61a7a9b7f'], // inflationRate, inflationDestination
         Staking: [1000], // LockPeriod
-    }
+        EraManager: [60 * 60 * 24], // 1 day
+        ServiceAgreementRegistry: [1e6], //threshold
+        PurchaseOfferMarket: [1e5, '0x0000000000000000000000000000000000000000'],
+    },
 };
