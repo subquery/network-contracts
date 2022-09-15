910bbef887fbebe26c5a8b322e09189e35d6af21
Jianzhuo Shen <shenjianzhuo@gmail.com>
2022-09-15 23:45:15 +0800

    update EraManager comments for docgen

--- contracts/EraManager.sol
+++ contracts/EraManager.sol
@@ -12,30 +12,44 @@ import './interfaces/IInflationController.sol';
 
 /**
  * @title EraManager contract
- * @dev Produce epochs based on a period to coordinate contracts
+ * @notice Produce epochs based on a period to coordinate contracts. Staking and reward distributing are running based on Eras
+ * @notice ### STATES
  */
 contract EraManager is Initializable, OwnableUpgradeable, IEraManager {
-    ISettings public settings;
-    uint256 public eraPeriod; // era period
-    uint256 public eraNumber; // current era number
-    uint256 public eraStartTime; // current era start time
 
+    /// @notice ISettings contract which stores SubQuery network contracts address
+    ISettings public settings;
+    /// @notice Era period in second
+    uint256 public eraPeriod; 
+    /// @notice Current Era number
+    uint256 public eraNumber; 
+    /// @notice Current era start time in unix timestamp
+    /// @notice ### EVENTS
+    uint256 public eraStartTime; 
+
+    /// @notice Emitted when admin update the eraPeriod
     event EraPeriodUpdate(uint256 indexed era, uint256 eraPeriod);
+    /// @notice Emitted when new Era started
+    /// @notice ### FUNCTIONS
     event NewEraStart(uint256 indexed era, address caller);
 
+    /**
+     * @notice Initialize the contract to start from Era 1
+     * @param _settings ISettings contract
+     * @param _eraPeriod eraPeriod in seconds
+     */
     function initialize(ISettings _settings, uint256 _eraPeriod) external initializer {
         __Ownable_init();
         require(_eraPeriod > 0, 'eraPeriod can not be 0');
 
         settings = _settings;
         eraPeriod = _eraPeriod;
-        // Emit start of era 1
         eraNumber = 1;
         emit NewEraStart(eraNumber, msg.sender);
     }
 
     /**
-     * @dev Start a new era if time already passed - anyone can call it
+     * @notice Start a new era if time already passed
      */
     function startNewEra() public {
         require(eraStartTime + eraPeriod < block.timestamp, 'Current era is still active');
@@ -49,6 +63,10 @@ contract EraManager is Initializable, OwnableUpgradeable, IEraManager {
         emit NewEraStart(eraNumber, msg.sender);
     }
 
+    /**
+     * @notice Start a new era if time already passed and return the new Era number
+     * @return eraNumber New Era number
+     */
     function safeUpdateAndGetEra() external returns (uint256) {
         if (eraStartTime + eraPeriod < block.timestamp) {
             startNewEra();
@@ -56,13 +74,19 @@ contract EraManager is Initializable, OwnableUpgradeable, IEraManager {
         return eraNumber;
     }
 
+    /**
+     * @notice Utility function to calculate the EraNumber from a given timestamp
+     * @param timestamp A given timestamp
+     * @return eraNumber The calculated Era number
+     */
     function timestampToEraNumber(uint256 timestamp) external view returns (uint256) {
         require(timestamp >= eraStartTime, 'only further timestamp available');
         return eraNumber + ((timestamp - eraStartTime) / eraPeriod);
     }
 
     /**
-     * @dev Update era period - only admin can call it
+     * @dev Update era period -  admin only
+     * @param newEraPeriod New Era Period to update
      */
     function updateEraPeriod(uint256 newEraPeriod) external onlyOwner {
         require(newEraPeriod > 0, 'new era can not be 0');
