2fd9d212307c147cad936d7cd38870353dbe165e
Jianzhuo Shen <shenjianzhuo@gmail.com>
2022-09-29 01:22:34 +0800

    update PermissionedExchange fuzz test

--- test-fuzz/PermissionedExchangeEchidnaTest.sol
+++ test-fuzz/PermissionedExchangeEchidnaTest.sol
@@ -14,17 +14,6 @@ contract PermissionedExchangeEchidnaTest {
     SQToken internal SQT;
     IERC20 internal USDC;
 
-    struct ExchangeOrder {
-        address tokenGive;
-        address tokenGet;
-        uint256 amountGive;
-        uint256 amountGet;
-        address sender;
-        uint256 expireDate;
-        uint256 pairOrderId;
-        uint256 tokenGiveBalance;
-    }
-
     constructor() {
         SQT = new SQToken(address(this));
         settings = new Settings();
@@ -46,18 +35,43 @@ contract PermissionedExchangeEchidnaTest {
         assert(z <= x); // check if there is a subtraction overflow
     }
 
+    /** test create a pairOrders, addQuota, trade, cancelOrder
+     * @param _agive amountGive
+     * @param _aget amountGet
+     * @param _ed expireDate
+     * @param _tgb tokenGiveBalance
+     * @param _qm tradeAmount
+     * createPairOrders(address(SQT), address(USDC), _agive, _aget, _ed, _tgb)
+     * first order: 
+     * tokenGive:           SQT Address                 
+     * tokenGet:            USDC Address 
+     * amountGive:          _agive
+     * amountGet:           _aget
+     * sender:              this address
+     * expireDate:          _ed
+     * pairOrderId:         next order id
+     * tokenGiveBalance:    _tgb
+     *
+     * next order
+     * tokenGive:           USDC Address 
+     * tokenGet:            SQT Address
+     * amountGive:          _aget
+     * amountGet:           _agive
+     * sender:              this address
+     * expireDate:          _ed
+     * pairOrderId:         first order id
+     * tokenGiveBalance:    0
+     */
     function test_init(uint256 _agive, uint256 _aget, uint256 _ed, uint256 _tgb, uint256 _qm) public {
         uint256 firstOrderId = pExchange.nextOrderId();
         pExchange.createPairOrders(address(SQT), address(USDC), _agive, _aget, _ed, _tgb);
         assert(pExchange.nextOrderId() == add(firstOrderId, 2));
-        // (address a,address b,uint256 c,uint256 d,address e,uint256 f,uint256 g,uint256 h) = pExchange.orders(firstOrderId);
-        // ExchangeOrder memory firstOrder = ExchangeOrder(a,b,c,d,e,f,g,h);
-        // (a,b,c,d,e,f,g,h) = pExchange.orders(add(firstOrderId, 1));
-        // ExchangeOrder memory nextOrder = ExchangeOrder(a,b,c,d,e,f,g,h);
-        // assert(firstOrder.tokenGive == address(SQT));
-        // assert(firstOrder.tokenGet == address(USDC));
-        // assert(nextOrder.tokenGive == address(USDC));
-        // assert(nextOrder.tokenGet == address(SQT));
+        (address ftokenGive,address ftokenGet,,,,,,) = pExchange.orders(firstOrderId);
+        (address ntokenGive,address ntokenGet,,,,,,) = pExchange.orders(add(firstOrderId, 1));
+        assert(ftokenGive == address(SQT));
+        assert(ftokenGet == address(USDC));
+        assert(ntokenGive == address(USDC));
+        assert(ntokenGet == address(SQT));
 
         test_addQuota(_qm);
 
@@ -67,14 +81,13 @@ contract PermissionedExchangeEchidnaTest {
     }
 
     function test_addQuota(uint256 qm) public {
+        uint256 quotaBefore = pExchange.tradeQuota(address(SQT), address(this));
         pExchange.addQuota(address(SQT), address(this), qm);
-        assert(pExchange.tradeQuota(address(SQT), address(this)) == qm);
+        assert(pExchange.tradeQuota(address(SQT), address(this)) == add(quotaBefore,qm));
     }
 
     function test_trade(uint256 id, uint256 amount) public {
         uint256 balanceBefore = SQT.balanceOf(address(pExchange));
-        //(address a,address b,uint256 c,uint256 d,address e,uint256 f,uint256 g,uint256 h) = pExchange.orders(id); 
-        //ExchangeOrder memory order = ExchangeOrder(a,b,c,d,e,f,g,h);
         (,,uint256 amountGive,uint256 amountGet,,,uint256 pairOrderId,uint256 tgbBefore) = pExchange.orders(id);
         (,,,,,,,uint256 ptgbBefore) = pExchange.orders(pairOrderId);
         uint256 newAmountGet = (amountGive * amount) / amountGet;
@@ -88,8 +101,6 @@ contract PermissionedExchangeEchidnaTest {
 
     function test_cancelOrder(uint256 id) public {
         uint256 balanceBefore = SQT.balanceOf(address(pExchange));
-        //(address a,address b,uint256 c,uint256 d,address e,uint256 f,uint256 g,uint256 h) = pExchange.orders(id); 
-        //ExchangeOrder memory order = ExchangeOrder(a,b,c,d,e,f,g,h);
         (,,,,,,uint256 pairOrderId, uint256 tgb) = pExchange.orders(id);
         pExchange.cancelOrder(id);
         assert(SQT.balanceOf(address(pExchange)) == sub(balanceBefore, tgb));
