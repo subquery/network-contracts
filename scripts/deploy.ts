5360885bed3bc9ffb151b542e8571e24229c88dd
Jianzhuo <67722296+Jianzhuo@users.noreply.github.com>
2023-03-01 18:20:39 +0800

    fix bugs (#151)

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,10 +5,10 @@ import setup from './setup';
 import {deployContracts} from './deployContracts';
 
 const main = async () => {
-    const {config, wallet, provider, overrides} = await setup(process.argv[2]);
+    const {name, config, wallet, provider, overrides} = await setup(process.argv[2]);
     const [deployment] = await deployContracts(wallet, config.contracts, overrides);
 
-    const filePath = `${__dirname}/../publish/${config.network.name}.json`;
+    const filePath = `${__dirname}/../publish/${name}.json`;
     writeFileSync(filePath, JSON.stringify(deployment, null, 4));
     console.log('Exported the deployment result to ', filePath);
 };
