0f573358a20ce90415052622a13b723174e09a97
Neo Sun <sun@subquery.network>
2023-05-02 14:00:59 +1200

    Add more const endpoints to rust sdk

--- rust/lib.rs
+++ rust/lib.rs
@@ -7,7 +7,46 @@ const KEPLER_ADDRESS: &str = include_str!("../publish/kepler.json");
 const TESTNET_ADDRESS: &str = include_str!("../publish/testnet.json");
 
 /// Default network that all services use now.
-pub const CURRENT_NETWORK: Network = Network::Testnet;
+pub const CURRENT_NETWORK: Network = Network::Kepler;
+
+const CHAIN_ICON_URL: &str = "https://icons.llamao.fi/icons/chains/rsz_polygon.jpg";
+const CHAIN_TOKEN_NAME: &str = "Matic Token";
+const CHAIN_TOKEN_SYMBOL: &str = "MATIC";
+const CHAIN_TOKEN_DECIMALS: i32 = 18;
+
+const BLOCK_EXPLORER_MAINNET: &str = "";
+const BLOCK_EXPLORER_KEPLER: &str = "https://polygonscan.com";
+const BLOCK_EXPLORER_TESTNET: &str = "https://mumbai.polygonscan.com";
+
+const CHAIN_ID_MAINNET: i32 = 137;
+const CHAIN_ID_KEPLER: i32 = 137;
+const CHAIN_ID_TESTNET: i32 = 80001;
+const CHAIN_NAME_MAINNET: &str = "";
+const CHAIN_NAME_KEPLER: &str = "Polygon";
+const CHAIN_NAME_TESTNET: &str = "Polygon Mumbai";
+
+const RPC_MAINNET1: &str = "";
+const RPC_MAINNET2: &str = "";
+const RPC_KEPLER1: &str =
+    "https://polygon.api.onfinality.io/rpc?apikey=e7acc294-c859-48ed-a742-5aadf0a084b9";
+const RPC_KEPLER2: &str = "https://polygon-rpc.com";
+const RPC_TESTNET1: &str =
+    "https://polygon-mumbai.api.onfinality.io/rpc?apikey=6b43efc3-a13c-4250-9203-e097fb9f239";
+const RPC_TESTNET2: &str = "https://rpc.ankr.com/polygon_mumbai";
+const RPC_TESTNET3: &str = "https://polygon-mumbai.infura.io/v3/4458cf4d1689497b9a38b1d6bbf05e78";
+
+const IPFS_MAINNET: &str = "";
+const IPFS_KEPLER: &str = "https://unauthipfs.subquery.network/ipfs/api/v0";
+const IPFS_TESTNET: &str = "https://interipfs.thechaindata.com/ipfs/api/v0";
+
+const SUBQL_MAINNET: &str = "";
+const SUBQL_KEPLER: &str = "https://api.subquery.network/sq/subquery/kepler-subquery-project";
+const SUBQL_TESTNET: &str =
+    "https://api.subquery.network/sq/subquery/kepler-subquery-project-mumbai";
+
+const EXPLORER_MAINNET: &str = "";
+const EXPLORER_KEPLER: &str = "https://kepler.subquery.network";
+const EXPLORER_TESTNET: &str = "https://kepler.thechaindata.com";
 
 /// Network types
 #[derive(Hash, Eq, PartialEq, Copy, Clone, Debug)]
@@ -41,6 +80,12 @@ pub struct NetworkConfig {
     pub icon_urls: Vec<String>,
     /// Native currency config
     pub native_currency: NetworkCurrency,
+    /// Subql network graphql endpoints
+    pub subql_urls: Vec<String>,
+    /// IPFS endpoints
+    pub ipfs_urls: Vec<String>,
+    /// Explorer endpoints
+    pub explorer_urls: Vec<String>,
 }
 
 impl Network {
@@ -65,70 +110,55 @@ impl Network {
     /// Get the network config
     pub fn config(&self) -> NetworkConfig {
         match self {
-            Network::Mainnet => {
-                NetworkConfig {
-                    chain_id: 137,
-                    chain_name: "Polygon".to_owned(),
-                    rpc_urls: vec![
-                        "https://polygon.api.onfinality.io/rpc?apikey=e7acc294-c859-48ed-a742-5aadf0a084b9".to_owned(),
-                        "https://polygon-rpc.com/".to_owned()
-                    ],
-                    icon_urls: vec![
-                        "https://icons.llamao.fi/icons/chains/rsz_polygon.jpg".to_owned()
-                    ],
-                    block_explorer_urls: vec![
-                        "https://polygonscan.com/".to_owned()
-                    ],
-                    native_currency: NetworkCurrency {
-                        name: "Matic Token".to_owned(),
-                        symbol: "MATIC".to_owned(),
-                        decimals: 18
-                    }
-                }
-            }
-            Network::Kepler => {
-                NetworkConfig {
-                    chain_id: 137,
-                    chain_name: "Polygon".to_owned(),
-                    rpc_urls: vec![
-                        "https://polygon.api.onfinality.io/rpc?apikey=e7acc294-c859-48ed-a742-5aadf0a084b9".to_owned(),
-                        "https://polygon-rpc.com/".to_owned()
-                    ],
-                    icon_urls: vec![
-                        "https://icons.llamao.fi/icons/chains/rsz_polygon.jpg".to_owned()
-                    ],
-                    block_explorer_urls: vec![
-                        "https://polygonscan.com/".to_owned()
-                    ],
-                    native_currency: NetworkCurrency {
-                        name: "Matic Token".to_owned(),
-                        symbol: "MATIC".to_owned(),
-                        decimals: 18
-                    }
-                }
-            }
-            Network::Testnet => {
-                NetworkConfig {
-                    chain_id: 80001,
-                    chain_name: "Mumbai".to_owned(),
-                    rpc_urls: vec![
-                        "https://polygon-mumbai.api.onfinality.io/rpc?apikey=6b43efc3-a13c-4250-9203-e097fb9f239".to_owned(),
-                        "https://rpc.ankr.com/polygon_mumbai".to_owned(),
-                        "https://polygon-mumbai.infura.io/v3/4458cf4d1689497b9a38b1d6bbf05e78".to_owned()
-                    ],
-                    icon_urls: vec![
-                        "https://icons.llamao.fi/icons/chains/rsz_polygon.jpg".to_owned()
-                    ],
-                    block_explorer_urls: vec![
-                        "https://mumbai.polygonscan.com/".to_owned()
-                    ],
-                    native_currency: NetworkCurrency {
-                        name: "Matic Token".to_owned(),
-                        symbol: "MATIC".to_owned(),
-                        decimals: 18
-                    }
-                }
-            }
+            Network::Mainnet => NetworkConfig {
+                chain_id: CHAIN_ID_MAINNET,
+                chain_name: CHAIN_NAME_MAINNET.to_owned(),
+                rpc_urls: vec![RPC_MAINNET1.to_owned(), RPC_MAINNET2.to_owned()],
+                icon_urls: vec![CHAIN_ICON_URL.to_owned()],
+                block_explorer_urls: vec![BLOCK_EXPLORER_MAINNET.to_owned()],
+                native_currency: NetworkCurrency {
+                    name: CHAIN_TOKEN_NAME.to_owned(),
+                    symbol: CHAIN_TOKEN_SYMBOL.to_owned(),
+                    decimals: CHAIN_TOKEN_DECIMALS.to_owned(),
+                },
+                subql_urls: vec![SUBQL_MAINNET.to_owned()],
+                ipfs_urls: vec![IPFS_MAINNET.to_owned()],
+                explorer_urls: vec![EXPLORER_MAINNET.to_owned()],
+            },
+            Network::Kepler => NetworkConfig {
+                chain_id: CHAIN_ID_KEPLER,
+                chain_name: CHAIN_NAME_KEPLER.to_owned(),
+                rpc_urls: vec![RPC_KEPLER1.to_owned(), RPC_KEPLER2.to_owned()],
+                icon_urls: vec![CHAIN_ICON_URL.to_owned()],
+                block_explorer_urls: vec![BLOCK_EXPLORER_KEPLER.to_owned()],
+                native_currency: NetworkCurrency {
+                    name: CHAIN_TOKEN_NAME.to_owned(),
+                    symbol: CHAIN_TOKEN_SYMBOL.to_owned(),
+                    decimals: CHAIN_TOKEN_DECIMALS.to_owned(),
+                },
+                subql_urls: vec![SUBQL_KEPLER.to_owned()],
+                ipfs_urls: vec![IPFS_KEPLER.to_owned()],
+                explorer_urls: vec![EXPLORER_KEPLER.to_owned()],
+            },
+            Network::Testnet => NetworkConfig {
+                chain_id: CHAIN_ID_TESTNET,
+                chain_name: CHAIN_NAME_TESTNET.to_owned(),
+                rpc_urls: vec![
+                    RPC_TESTNET1.to_owned(),
+                    RPC_TESTNET2.to_owned(),
+                    RPC_TESTNET3.to_owned(),
+                ],
+                icon_urls: vec![CHAIN_ICON_URL.to_owned()],
+                block_explorer_urls: vec![BLOCK_EXPLORER_TESTNET.to_owned()],
+                native_currency: NetworkCurrency {
+                    name: CHAIN_TOKEN_NAME.to_owned(),
+                    symbol: CHAIN_TOKEN_SYMBOL.to_owned(),
+                    decimals: CHAIN_TOKEN_DECIMALS.to_owned(),
+                },
+                subql_urls: vec![SUBQL_TESTNET.to_owned()],
+                ipfs_urls: vec![IPFS_TESTNET.to_owned()],
+                explorer_urls: vec![EXPLORER_TESTNET.to_owned()],
+            },
         }
     }
 }
