e8d2be61af9496cc985c8c609e8c10a51cc66953
HZHAIX <zhaihaohk@gmail.com>
2023-06-08 21:06:15 +1200

    Upgrade exchange contract (#204)
    * Upgrade exchange contract
    
    * Fix deployments file
    
    * Fix contract init in test
    
    * Uncomment upgrade contract script

--- src/deployments.ts
+++ src/deployments.ts
@@ -2,14 +2,13 @@
 
 import { ContractDeployment, SubqueryNetwork } from './types';
 // The path is compatible with `build` folder
-import mainnetDeployment from '../publish/mainnet.json';
-import keplerDeployment from '../publish/kepler.json';
-import testnetDeployment from '../publish/testnet.json';
-import localDeployment from '../publish/local.json';
+import mainnetDeployment from './publish/mainnet.json';
+import keplerDeployment from './publish/kepler.json';
+import testnetDeployment from './publish/testnet.json';
 
 export const DEPLOYMENT_DETAILS: Record<SubqueryNetwork, ContractDeployment> = {
   mainnet: mainnetDeployment,
   kepler: keplerDeployment,
   testnet: testnetDeployment,
-  local: localDeployment,
+  local: testnetDeployment,
 };
