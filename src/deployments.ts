77f962a9609303264c480f01ffdf02686e075636
HZ <zhaihaohk@gmail.com>
2023-07-04 12:37:11 +1200

    Update plan template config for Testnet

--- src/deployments.ts
+++ src/deployments.ts
@@ -2,8 +2,8 @@
 
 import { ContractDeployment, SubqueryNetwork } from './types';
 
-// import moduleAlias from 'module-alias';
-// moduleAlias.addAlias('./publish', '../publish');
+import moduleAlias from 'module-alias';
+moduleAlias.addAlias('./publish', '../publish');
 
 import mainnetDeployment from './publish/mainnet.json';
 import keplerDeployment from './publish/kepler.json';
