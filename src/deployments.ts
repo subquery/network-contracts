8d9be9b7362a94a6b8293d4a76c7c84c6aac8087
mzxyz <mzxyz>
2023-08-11 11:24:09 +1200

    fix: import issue

--- src/deployments.ts
+++ src/deployments.ts
@@ -2,11 +2,11 @@
 
 import { ContractDeployment, SubqueryNetwork } from './types';
 
-// import moduleAlias from 'module-alias';
-// moduleAlias.addAlias('./publish', '../publish');
+import moduleAlias from 'module-alias';
+moduleAlias.addAlias('./publish', '../publish');
 
-import mainnetDeployment from './publish/mainnet.json';
 import keplerDeployment from './publish/kepler.json';
+import mainnetDeployment from './publish/mainnet.json';
 import testnetDeployment from './publish/testnet.json';
 
 export const DEPLOYMENT_DETAILS: Record<SubqueryNetwork, ContractDeployment> = {
