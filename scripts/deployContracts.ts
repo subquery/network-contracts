b1f462446afd68302a27ac71e849e63a52510cf1
mzxyz <8177474+mzxyz@users.noreply.github.com>
2023-08-18 14:05:48 +1200

    Deploy new contracts on Testnet (#228)
    * Update deployContract script
    
    * Deploy new contracts on Testnet
    
    * Redeploy Settings and VSQToken with owner_init
    
    * Fix test cases

--- scripts/deployContracts.ts
+++ scripts/deployContracts.ts
@@ -202,9 +202,9 @@ export async function deployContracts(
     try {
         const proxyAdmin = await deployContract<ProxyAdmin>('ProxyAdmin');
 
-        const settings = await deployContract<Settings>('Settings');
-
+        const settings = await deployContract<Settings>('Settings', { proxyAdmin, initConfig: [] });
         const settingsAddress = settings.address;
+
         const inflationController = await deployContract<InflationController>('InflationController', {
             initConfig: [settingsAddress],
             proxyAdmin,
@@ -216,7 +216,7 @@ export async function deployContracts(
         });
 
         // deploy VSQToken contract
-        const vsqtToken = await deployContract<VSQToken>('VSQToken', { initConfig: [settingsAddress] });
+        const vsqtToken = await deployContract<VSQToken>('VSQToken', { proxyAdmin, initConfig: [settingsAddress] });
 
         //deploy Airdropper contract
         const [settleDestination] = config['Airdropper'];
