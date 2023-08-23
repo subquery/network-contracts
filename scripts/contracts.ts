15873e292a9b0534284639c453eb57c1db194cf5
mzxyz <8177474+mzxyz@users.noreply.github.com>
2023-08-23 23:29:55 +1200

    Update `UPGRADEBAL_CONTRACTS`

--- scripts/contracts.ts
+++ scripts/contracts.ts
@@ -1,66 +1,65 @@
-import {Wallet} from '@ethersproject/wallet';
-import {BaseContract, BigNumber, ContractFactory, Signer} from 'ethers';
-import {Provider} from '@ethersproject/abstract-provider';
+import { Provider } from '@ethersproject/abstract-provider';
+import { Wallet } from '@ethersproject/wallet';
+import { BaseContract, BigNumber, ContractFactory, Signer } from 'ethers';
 
 import CONTRACTS from '../src/contracts';
 
 import {
+    Airdropper,
+    Airdropper__factory,
+    ConsumerHost,
+    ConsumerHost__factory,
+    ConsumerRegistry,
+    ConsumerRegistry__factory,
+    ContractName,
+    DisputeManager,
+    DisputeManager__factory,
+    EraManager,
+    EraManager__factory,
+    IndexerRegistry,
+    IndexerRegistry__factory,
+    InflationController,
+    InflationController__factory,
+    PermissionedExchange,
+    PermissionedExchange__factory,
+    PlanManager,
+    PlanManager__factory,
+    PriceOracle,
+    PriceOracle__factory,
     ProxyAdmin,
     ProxyAdmin__factory,
-    AdminUpgradeabilityProxy__factory,
-    InflationController__factory,
-    Staking__factory,
-    IndexerRegistry__factory,
-    QueryRegistry__factory,
-    InflationController,
-    Staking,
-    StakingManager,
-    StakingManager__factory,
-    Settings__factory,
-    QueryRegistry,
-    PlanManager__factory,
-    SQToken__factory,
-    ServiceAgreementRegistry__factory,
-    ServiceAgreementRegistry,
-    EraManager__factory,
-    PurchaseOfferMarket__factory,
-    Settings,
-    SQToken,
-    VSQToken,
-    VSQToken__factory,
-    EraManager,
-    IndexerRegistry,
-    PlanManager,
     PurchaseOfferMarket,
+    PurchaseOfferMarket__factory,
+    QueryRegistry,
+    QueryRegistry__factory,
     RewardsDistributer,
     RewardsDistributer__factory,
+    RewardsHelper,
+    RewardsHelper__factory,
     RewardsPool,
     RewardsPool__factory,
     RewardsStaking,
     RewardsStaking__factory,
-    RewardsHelper,
-    RewardsHelper__factory,
+    SQToken,
+    SQToken__factory,
+    ServiceAgreementRegistry,
+    ServiceAgreementRegistry__factory,
+    Settings,
+    Settings__factory,
+    Staking,
+    StakingManager,
+    StakingManager__factory,
+    Staking__factory,
     StateChannel,
     StateChannel__factory,
-    Airdropper,
-    Airdropper__factory,
-    PermissionedExchange,
-    PermissionedExchange__factory,
+    VSQToken,
+    VSQToken__factory,
     Vesting,
     Vesting__factory,
-    ConsumerHost,
-    ConsumerHost__factory,
-    DisputeManager,
-    DisputeManager__factory,
-    ConsumerRegistry__factory,
-    ConsumerRegistry,
-    PriceOracle,
-    PriceOracle__factory,
-    ContractName,
 } from '../src';
 
 export interface FactoryContstructor {
-    new (wallet: Wallet): ContractFactory;
+    new(wallet: Wallet): ContractFactory;
     connect: (address: string, signerOrProvider: Signer | Provider) => BaseContract;
     readonly abi: any;
 }
@@ -93,28 +92,30 @@ export type Contracts = {
     priceOracle: PriceOracle;
 };
 
-export const UPGRADEBAL_CONTRACTS: Partial<Record<keyof typeof CONTRACTS, [{bytecode: string}, FactoryContstructor]>> =
-    {
-        InflationController: [CONTRACTS.InflationController, InflationController__factory],
-        IndexerRegistry: [CONTRACTS.IndexerRegistry, IndexerRegistry__factory],
-        PlanManager: [CONTRACTS.PlanManager, PlanManager__factory],
-        QueryRegistry: [CONTRACTS.QueryRegistry, QueryRegistry__factory],
-        RewardsDistributer: [CONTRACTS.RewardsDistributer, RewardsDistributer__factory],
-        RewardsPool: [CONTRACTS.RewardsPool, RewardsPool__factory],
-        RewardsStaking: [CONTRACTS.RewardsStaking, RewardsStaking__factory],
-        RewardsHelper: [CONTRACTS.RewardsHelper, RewardsHelper__factory],
-        ServiceAgreementRegistry: [CONTRACTS.ServiceAgreementRegistry, ServiceAgreementRegistry__factory],
-        Staking: [CONTRACTS.Staking, Staking__factory],
-        StakingManager: [CONTRACTS.StakingManager, StakingManager__factory],
-        EraManager: [CONTRACTS.EraManager, EraManager__factory],
-        PurchaseOfferMarket: [CONTRACTS.PurchaseOfferMarket, PurchaseOfferMarket__factory],
-        StateChannel: [CONTRACTS.StateChannel, StateChannel__factory],
+export const UPGRADEBAL_CONTRACTS: Partial<Record<keyof typeof CONTRACTS, [{ bytecode: string }, FactoryContstructor]>> =
+{
+    InflationController: [CONTRACTS.InflationController, InflationController__factory],
+    IndexerRegistry: [CONTRACTS.IndexerRegistry, IndexerRegistry__factory],
+    PlanManager: [CONTRACTS.PlanManager, PlanManager__factory],
+    QueryRegistry: [CONTRACTS.QueryRegistry, QueryRegistry__factory],
+    RewardsDistributer: [CONTRACTS.RewardsDistributer, RewardsDistributer__factory],
+    RewardsPool: [CONTRACTS.RewardsPool, RewardsPool__factory],
+    RewardsStaking: [CONTRACTS.RewardsStaking, RewardsStaking__factory],
+    RewardsHelper: [CONTRACTS.RewardsHelper, RewardsHelper__factory],
+    ServiceAgreementRegistry: [CONTRACTS.ServiceAgreementRegistry, ServiceAgreementRegistry__factory],
+    Staking: [CONTRACTS.Staking, Staking__factory],
+    StakingManager: [CONTRACTS.StakingManager, StakingManager__factory],
+    EraManager: [CONTRACTS.EraManager, EraManager__factory],
+    PurchaseOfferMarket: [CONTRACTS.PurchaseOfferMarket, PurchaseOfferMarket__factory],
+    StateChannel: [CONTRACTS.StateChannel, StateChannel__factory],
 
-        PermissionedExchange: [CONTRACTS.PermissionedExchange, PermissionedExchange__factory],
-        ConsumerHost: [CONTRACTS.ConsumerHost, ConsumerHost__factory],
-        DisputeManager: [CONTRACTS.DisputeManager, DisputeManager__factory],
-        ConsumerRegistry: [CONTRACTS.ConsumerRegistry, ConsumerRegistry__factory],
-    };
+    PermissionedExchange: [CONTRACTS.PermissionedExchange, PermissionedExchange__factory],
+    ConsumerHost: [CONTRACTS.ConsumerHost, ConsumerHost__factory],
+    DisputeManager: [CONTRACTS.DisputeManager, DisputeManager__factory],
+    ConsumerRegistry: [CONTRACTS.ConsumerRegistry, ConsumerRegistry__factory],
+    PriceOracle: [CONTRACTS.PriceOracle, PriceOracle__factory],
+    Settings: [CONTRACTS.Settings, Settings__factory],
+};
 
 export const CONTRACT_FACTORY: Record<ContractName, FactoryContstructor> = {
     ProxyAdmin: ProxyAdmin__factory,
