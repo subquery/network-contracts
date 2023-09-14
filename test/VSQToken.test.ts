7f28fe50e41a5b32891e54bec23782b6006e9f27
mzxyz <8177474+mzxyz@users.noreply.github.com>
2023-09-15 08:59:29 +1200

    Fix other tests

--- test/VSQToken.test.ts
+++ test/VSQToken.test.ts
@@ -1,12 +1,12 @@
 // Copyright (C) 2020-2022 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import {expect} from 'chai';
-import {ethers, waffle} from 'hardhat';
+import { expect } from 'chai';
+import { ethers, waffle } from 'hardhat';
 
-import {IndexerRegistry, EraManager, SQToken, Staking, VSQToken, StakingManager} from '../src';
-import {deployContracts} from './setup';
-import {registerIndexer, startNewEra, etherParse} from './helper';
+import { EraManager, IndexerRegistry, SQToken, Staking, StakingManager, VSQToken } from '../src';
+import { etherParse, registerIndexer, startNewEra } from './helper';
+import { deployContracts } from './setup';
 
 describe('VSQToken Contract', () => {
     const mockProvider = waffle.provider;
@@ -17,6 +17,8 @@ describe('VSQToken Contract', () => {
     let eraManager: EraManager;
     let indexerRegistry: IndexerRegistry;
     let vtoken: VSQToken;
+
+    const amount = '2000';
 
     beforeEach(async () => {
         [root, indexer, indexer2, delegator] = await ethers.getSigners();
@@ -29,9 +31,9 @@ describe('VSQToken Contract', () => {
         vtoken = deployment.vtoken;
 
         //register indexer1: Indexer1 balance: 10 sqt, Indexer1 staked amount: 10 sqt
-        await registerIndexer(token, indexerRegistry, staking, root, indexer, '2000');
+        await registerIndexer(token, indexerRegistry, staking, root, indexer, amount);
         //register indexer2: Indexer2 balance: 10 sqt, Indexer2 staked amount: 10 sqt
-        await registerIndexer(token, indexerRegistry, staking, root, indexer2, '2000');
+        await registerIndexer(token, indexerRegistry, staking, root, indexer2, amount);
         //setup delegator: delegator balance: 15 sqt
         await token.connect(root).transfer(delegator.address, etherParse('15'));
         await token.connect(delegator).increaseAllowance(staking.address, etherParse('15'));
@@ -40,10 +42,10 @@ describe('VSQToken Contract', () => {
     it('get balance of VSQT Token should work', async () => {
         await stakingManager.connect(delegator).delegate(indexer.address, etherParse('5'));
         await stakingManager.connect(delegator).delegate(indexer2.address, etherParse('5'));
-        expect(await token.balanceOf(indexer.address)).to.equal(etherParse('1000'));
-        expect(await vtoken.balanceOf(indexer.address)).to.equal(etherParse('2000'));
-        expect(await token.balanceOf(indexer2.address)).to.equal(etherParse('1000'));
-        expect(await vtoken.balanceOf(indexer2.address)).to.equal(etherParse('2000'));
+        expect(await token.balanceOf(indexer.address)).to.equal(etherParse(amount));
+        expect(await vtoken.balanceOf(indexer.address)).to.equal(etherParse(amount));
+        expect(await token.balanceOf(indexer2.address)).to.equal(etherParse(amount));
+        expect(await vtoken.balanceOf(indexer2.address)).to.equal(etherParse(amount));
         expect(await token.balanceOf(delegator.address)).to.equal(etherParse('5'));
         expect(await vtoken.balanceOf(delegator.address)).to.equal(etherParse('15'));
 
