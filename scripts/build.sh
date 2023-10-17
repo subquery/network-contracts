c02a619d451156d03715c39fc0b15e8d28f40479
mzxyz <8177474+mzxyz@users.noreply.github.com>
2023-10-17 16:40:43 +1300

    Fix build issue

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-PROXY_ADMIN_PATH=./artifacts/contracts/ProxyAdmin.sol
+PROXY_ADMIN_PATH=artifacts/contracts/ProxyAdmin.sol
 mkdir -p $PROXY_ADMIN_PATH
-cp ./node_modules/@openzeppelin/contracts/build/contracts/ProxyAdmin.json $PROXY_ADMIN_PATH/ProxyAdmin.json
+cp node_modules/@openzeppelin/contracts/build/contracts/ProxyAdmin.json $PROXY_ADMIN_PATH/ProxyAdmin.json
 
 mkdir -p build/build/publish
 cp -r src/* build/
