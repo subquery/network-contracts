24dfbfc9e32d0fdf781a52562e0c6c2c87612f2a
mzxyz <8177474+mzxyz@users.noreply.github.com>
2023-12-12 17:31:36 +1300

    Update token names

--- contracts/root/VTSQtoken.sol
+++ contracts/root/VTSQtoken.sol
@@ -17,7 +17,7 @@ contract VTSQToken is ERC20, Ownable, ERC20Burnable {
         _;
     }
 
-    constructor(address _minter, uint256 totalSupply) ERC20('SubQueryToken', 'SQT') Ownable() {
+    constructor(address _minter, uint256 totalSupply) ERC20('VTSubQueryToken', 'vtSQT') Ownable() {
         minter = _minter;
         _mint(msg.sender, totalSupply);
     }
